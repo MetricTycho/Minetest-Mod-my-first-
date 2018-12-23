52b4495379cbd560f1279af596be1975a0513883
MetricTycho <46092636+MetricTycho@users.noreply.github.com>
2018-12-23 22:40:50 +0100

    Update init.lua
    Mit Crafting

--- lizium/init.lua
+++ lizium/init.lua
@@ -2,3 +2,12 @@ minetest.register_node("lizium:lizium", {
 	tiles = {"lizium.png"},
 	description = ("lizium ore"),
 	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},})
+
+minetest.register_craft({
+	output = 'fire:basic_flame',
+	recipe = {
+		{'', '', ''},
+		{'default:water_source', 'test:blau', ''},
+		{'', '', ''},
+	}
+})
