8dee1831cadc6c4f48d6481491e2b8ecfb3b46a2
MetricTycho <46092636+MetricTycho@users.noreply.github.com>
2019-02-02 13:41:16 +0100

    Umgeschrieben
    mit Rezept

--- lizium/init.lua
+++ lizium/init.lua
@@ -2,3 +2,13 @@ minetest.register_node("lizium:lizium", {
 	tiles = {"lizium.png"},
 	description = ("lizium ore"),
 	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = 'fire:basic_flame',
+	recipe = {'default:water_source', 'lizium:lizium'},
+	replacements = {
+		{"lizium:lizium", "default:water_source"}
+	}
+})
+
